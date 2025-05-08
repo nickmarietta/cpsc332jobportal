8f4f8d00893e89d6d043bbef65215b7f6bf8149b
nickmarietta <nick.marietta@csu.fullerton.edu>
2025-05-08 11:02:19 -0700

    updating the DB connection

--- db.php
+++ db.php
@@ -9,6 +9,6 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
-    die("❌ Database connection failed: " . $conn->connect_error);
+    die("Database connection failed: " . $conn->connect_error);
 }
 ?>
