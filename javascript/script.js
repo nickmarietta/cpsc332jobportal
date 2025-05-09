1bfa74bb53a3b4a8ad491b8fc96b08b7e0929534
JoshPavon <143138185+JoshPavon@users.noreply.github.com>
2025-05-08 17:17:32 -0700

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,10 @@
 
+// Job selector script
+const sortBtns = document.querySelectorAll(".job-id > *")
+
+sortBtns.forEach((btn) =>{
+    btn.addEventListener('click', () =>{
+        sortBtns.forEach((btn) => btn.classList.remove("active"));
+        btn.classList.add("active");
+    })
+})
